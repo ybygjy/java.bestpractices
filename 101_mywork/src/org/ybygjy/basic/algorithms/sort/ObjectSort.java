e4b563e935db1478e532d26404f61d1164c06cde
ybygjy <ybygjy@gmail.com>
2014-03-28 10:07:55 +0800

    排序算法

---
+++ 101_mywork/src/org/ybygjy/basic/algorithms/sort/ObjectSort.java
@@ -0,0 +1,78 @@
+package org.ybygjy.basic.algorithms.sort;
+
+import java.util.Arrays;
+
+/**
+ * ∂‘œÛ≈≈–Ú
+ * @author WangYanCheng
+ * @version 2014-3-27
+ */
+public class ObjectSort {
+    public static void main(String[] args) {
+        ArrayInOb personArray = new ArrayInOb(100);
+        personArray.insert("A", "John", 10);
+        personArray.insert("B", "Jams", 40);
+        personArray.insert("D", "Jason", 20);
+        personArray.insert("C", "William", 40);
+        System.out.println(personArray.toString());
+        personArray.insertionSort();
+        System.out.println(personArray.toString());
+    }
+}
+class Person {
+    private String lastName;
+    private String firstName;
+    private int age;
+    public Person(String lastName, String firstName, int age) {
+        super();
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.age = age;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [lastName=" + lastName + ", firstName=" + firstName + ", age=" + age + "]";
+    }
+}
+class ArrayInOb {
+    private Person[] array;
+    //Number of data items
+    private int nElems;
+    public ArrayInOb(int nElems) {
+        array = new Person[nElems];
+        this.nElems = 0;
+    }
+    public void insert(String lastName, String firstName, int age) {
+        array[nElems++] = new Person(lastName, firstName, age);
+    }
+    /**
+     * ≤Â»Î≈≈–Ú
+     */
+    public void insertionSort() {
+        int out;
+        int in;
+        int len = nElems;
+        Person tmpPerson = null;
+        for (out = 1;out < len; out++) {
+            tmpPerson = this.array[out];
+            in = out;
+            while (in > 0 && this.array[in - 1].getLastName().compareTo(tmpPerson.getLastName()) > 0) {
+                this.array[in] = this.array[in - 1];
+                --in;
+            }
+            this.array[in] = tmpPerson;
+        }
+    }
+    @Override
+    public String toString() {
+        return "ArrayInOb [array=" + Arrays.toString(array) + "]";
+    }
+}
