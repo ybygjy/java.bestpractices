8e4e5dd89849a94cd70c4bc464a1e49efb2ef2c4
ybygjy <ybygjy@gmail.com>
2014-04-23 22:21:02 +0800

    synchronized

---
+++ 101_mywork/src/org/ybygjy/fileEdit/AutomaticFillPackage4Class.java
@@ -0,0 +1,112 @@
+package org.ybygjy.fileEdit;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 分析Class类路径，自动填充包名称
+ * @author WangYanCheng
+ * @version 2014-4-22
+ */
+public class AutomaticFillPackage4Class {
+    /**基目录*/
+    private File sourceDir;
+    private String fileEncode = "UTF-8";
+    public AutomaticFillPackage4Class() {
+        sourceDir = new File("D:\\DEV\\02_work\\02_financial_server_v1\\src\\main\\java");
+        if (!sourceDir.isDirectory()) {
+            throw new RuntimeException("必须指定文件夹！");
+        }
+    }
+    public void doWork() {
+        innerDoWork(sourceDir);
+    }
+    /**
+     * 遍历目录取.java文件
+     * @param file
+     */
+    private void innerDoWork(File file) {
+        //1、取目录下子文件
+        File[] tmpFiles = file.listFiles(new CustomerFileFilter());
+        //2、验证文件是否可读/写，是否是.java文件
+        for (File tmpFile : tmpFiles) {
+            if (tmpFile.isDirectory()) {
+                innerDoWork(tmpFile);
+            } else if (tmpFile.canWrite()) {
+                doUpdateFilePackage(tmpFile);
+            }
+        }
+    }
+    /**
+     * 更新文件package
+     * @param tmpFile
+     */
+    private void doUpdateFilePackage(File tmpFile) {
+        //2.1、取.java文件内容匹配package一行
+        //2.2、分析.java文件路径，取package包名称
+        //2.3、利用正则表达式进行替换
+        //2.4、写入文件，刷新缓冲流
+        BufferedInputStream rafInst = null;
+        ByteArrayOutputStream baos = null;
+        FileOutputStream fout = null;
+        try {
+            rafInst = new BufferedInputStream(new FileInputStream(tmpFile));
+            baos = new ByteArrayOutputStream();
+            byte[] cbuf = new byte[2048];
+            int flag = -1;
+            //加载文件内容
+            while ((flag = rafInst.read(cbuf)) != -1) {
+                baos.write(cbuf, 0, flag);
+            }
+            String fileContent = baos.toString(fileEncode);
+            rafInst.close();
+            baos.close();
+            //分析文件路径提取包路径
+            String pagepath = "package " + analysisPackagePath(tmpFile) + ";";
+            //替换/写入
+            fileContent = fileContent.replaceFirst("package([^\r\n]+)", pagepath);
+            fout = new FileOutputStream(tmpFile, false);
+            fout.write(fileContent.getBytes(fileEncode));
+            fout.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * 分析文件路径取package路径
+     * @param tmpFile
+     * @return
+     */
+    private String analysisPackagePath(File tmpFile) {
+        String filePath = tmpFile.getParentFile().getPath();
+        filePath =
+            Pattern.compile(File.separator, Pattern.LITERAL).matcher(filePath).replaceAll(Matcher.quoteReplacement("."));
+        Matcher matcher = Pattern.compile(".*(com(\\.\\w+)+)").matcher(filePath);
+        String rtnpackagePath = filePath;
+        if (matcher.find()) {
+            rtnpackagePath = matcher.group(1);
+        }
+        return rtnpackagePath;
+    }
+    public static void main(String[] args) {
+        new AutomaticFillPackage4Class().doWork();
+    }
+}
+/**
+ * 文件过滤
+ * @author WangYanCheng
+ * @version 2014-4-22
+ */
+class CustomerFileFilter implements FileFilter {
+    @Override
+    public boolean accept(File fileInst) {
+        return fileInst.isDirectory() || fileInst.getName().matches(".*\\.java$");
+    }
+    
+}
---
+++ 101_mywork/src/org/ybygjy/util/blog/BlogRank4HttpClient4.java
@@ -0,0 +1,216 @@
+package org.ybygjy.util.blog;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethodBase;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.SimpleHttpConnectionManager;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+/**
+ * 利用HttpClient下载Blog内容
+ * @author WangYanCheng
+ * @version 2014-4-22
+ */
+public class BlogRank4HttpClient4 {
+    private String blogSit = "http://blog.csdn.net";
+    private Map<String, String> requestParam = new HashMap<String, String>();
+    private Map<String, String> requestHeaderParam = new HashMap<String, String>();
+    private AtomicInteger pageCount;
+    private AtomicInteger articleCount;
+    private HttpClient httpClient;
+    private List<String> cachePage;
+    public BlogRank4HttpClient4() {
+        httpClient = new HttpClient(new SimpleHttpConnectionManager(false));
+        cachePage = new ArrayList<String>();
+        pageCount = new AtomicInteger(0);
+        articleCount = new AtomicInteger(0);
+    }
+    public void doWork() {
+        doRequest(blogSit + "/ybygjy");
+        System.out.println("共处理\r\n分页：" + pageCount.get() + "\t文章：" + articleCount.get());
+    }
+    /**
+     * 请求指定URI
+     * @param uri
+     */
+    public void doRequest(String uri) {
+        HttpMethodBase httpMethod = createMethod(uri);
+        try {
+            int respCode = httpClient.executeMethod(httpMethod);
+System.out.println(httpMethod.getURI() + "=>" + respCode);
+            String responseCharset = httpMethod.getResponseCharSet();
+            String htmlContent = new String(httpMethod.getResponseBody(), 0, (int) httpMethod.getResponseContentLength(), responseCharset);
+            new HTMLParser(htmlContent, new HTMLParserCallback() {
+                @Override
+                public void afterParseUri(String[] pageUrls, String[] articleUrls) {
+                    //访问分页，需要记录当前是第几页，防止出现递归，把内容存储在HttpHeader中
+                    if (null != pageUrls) {
+                        for (String pageUrl : pageUrls) {
+                            String hashCode = String.valueOf(pageUrl.hashCode());
+                            int flag = cachePage.indexOf(hashCode);
+                            if (flag == -1) {
+                                cachePage.add(hashCode);
+                                pageCount.incrementAndGet();
+                                doRequest(blogSit + pageUrl);
+                            }
+                        }
+                    }
+                    if (null != articleUrls) {
+                        for (String articleUrl : articleUrls) {
+                            String hashCode = String.valueOf(articleUrl.hashCode());
+                            int flag = cachePage.indexOf(hashCode);
+                            if (flag == -1) {
+                                cachePage.add(hashCode);
+                                articleCount.incrementAndGet();
+                                doRequest(blogSit + articleUrl);
+                            }
+                        }
+                    }
+                }
+            }).doParseHTML();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * 创建请求实体
+     * @return
+     */
+    private HttpMethodBase createMethod(String uri) {
+        HttpMethodBase httpMethod = new PostMethod(uri);
+        addRequestHeaderParam(httpMethod);
+        addRequestParam(httpMethod);
+        return httpMethod;
+    }
+    /**
+     * 添加请求参数
+     * @param httpMethod
+     */
+    private void addRequestParam(HttpMethodBase httpMethod) {
+        List<NameValuePair> valuePairCollection = new ArrayList<NameValuePair>(); 
+        for (Map.Entry<String, String> entry : requestParam.entrySet()) {
+            valuePairCollection.add(new NameValuePair(entry.getKey(), entry.getValue()));
+        }
+        httpMethod.setQueryString(valuePairCollection.toArray(new NameValuePair[valuePairCollection.size()]));
+    }
+    /**
+     * 添加请求头参数
+     * @param httpMethod
+     */
+    private void addRequestHeaderParam(HttpMethodBase httpMethod) {
+        for (Map.Entry<String, String> entry : requestHeaderParam.entrySet()) {
+            httpMethod.addRequestHeader(entry.getKey(), entry.getValue());
+        }
+    }
+    /**
+     * 添加参数
+     * @param key
+     * @param value
+     */
+    public void addParam(String key, String value) {
+        requestParam.put(key, value);
+    }
+    /**
+     * 删除参数
+     * @param key
+     * @return
+     */
+    public String removeParam(String key) {
+        return requestParam.remove(key);
+    }
+    /**
+     * 添加请求头参数
+     * @param key
+     * @param value
+     */
+    public void addRequestHeaderParam(String key, String value) {
+        requestHeaderParam.put(key, value);
+    }
+    /**
+     * 移除请求头参数
+     * @param key
+     * @return
+     */
+    public String removeRequestHeaderParam(String key) {
+        return requestHeaderParam.remove(key);
+    }
+    /**
+     * 程序入口
+     * @param args 参数列表
+     */
+    public static void main(String[] args) {
+        BlogRank4HttpClient4 blogRank = new BlogRank4HttpClient4();
+        blogRank.addRequestHeaderParam("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+        blogRank.addRequestHeaderParam("Accept-Language", "zh-CN,zh;q=0.8,en;q=0.6");
+        blogRank.addRequestHeaderParam("Cache-Control", "no-cache");
+        blogRank.addRequestHeaderParam("User-Agent", "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36");
+        blogRank.addParam("viewmode", "contents");
+        blogRank.doWork();
+    }
+}
+/**
+ * 此类负责解析HTML提取特定HTML内容
+ * @author WangYanCheng
+ * @version 2014-4-22
+ */
+class HTMLParser {
+    private String htmlContent;
+    private List<String> pageUrl;
+    private List<String> articleUrl;
+    private HTMLParserCallback htmlParseCallback;
+    public HTMLParser(String htmlContent, HTMLParserCallback htmlParseCallback) {
+        this.htmlContent = htmlContent;
+        this.pageUrl = new ArrayList<String>();
+        this.articleUrl = new ArrayList<String>();
+        this.htmlParseCallback = htmlParseCallback;
+    }
+    /**
+     * 负责解析、转储HTML特定内容
+     * @param htmlContent
+     */
+    public void doParseHTML() {
+        parseURI(htmlContent);
+    }
+    /**
+     * 解析逻辑
+     * @param htmlContent
+     */
+    private void parseURI(String htmlContent) {
+        String regexStr = "<a\\shref=\"(/[^\"]+)\">";
+        Matcher matcher = Pattern.compile(regexStr).matcher(htmlContent);
+        while (matcher.find()) {
+            //验证是否是分页数据
+            String articleUri = matcher.group(1);
+            if (articleUri.matches(".*list/\\d+")) {
+                pageUrl.add(articleUri);
+            } else {
+                articleUrl.add(articleUri);
+            }
+        }
+        if (null != htmlParseCallback) {
+            htmlParseCallback.afterParseUri(pageUrl.toArray(new String[pageUrl.size()]), articleUrl.toArray(new String[articleUrl.size()]));
+        }
+    }
+    public List<String> getPageUrl() {
+        return pageUrl;
+    }
+    public List<String> getArticleUrl() {
+        return articleUrl;
+    }
+}
+/**
+ * HTML解析回调接口
+ * @author WangYanCheng
+ * @version 2014-4-22
+ */
+interface HTMLParserCallback {
+    public void afterParseUri(String[] pageUrls, String [] articleUrls);
+}
