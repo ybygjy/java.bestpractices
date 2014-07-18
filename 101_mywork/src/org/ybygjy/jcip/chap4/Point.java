2d01bb4d5072ec1193e36c18e13c5a898a4bdde4
ybygjy <ybygjy@gmail.com>
2014-07-18 18:05:49 +0800

    Learn JCIP

---
+++ 101_mywork/src/org/ybygjy/jcip/chap4/Point.java
@@ -0,0 +1,15 @@
+package org.ybygjy.jcip.chap4;
+
+/**
+ * ¶şÎ¬Î»ÖÃ
+ * @author WangYanCheng
+ * @version 2014-7-18
+ */
+public class Point {
+    public final int x;
+    public final int y;
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
