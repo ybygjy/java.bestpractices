2471d893781c08493cc58066b481a0eb3483e62e
ybygjy <ybygjy@gmail.com>
2014-07-30 22:47:50 +0800

    Learn JCIP

--- 101_mywork/src/org/ybygjy/jcip/chap5/ConcurrentMapLockStriping.java
+++ 101_mywork/src/org/ybygjy/jcip/chap5/ConcurrentMapLockStriping.java
@@ -1,5 +1,8 @@
 package org.ybygjy.jcip.chap5;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 
@@ -18,6 +21,7 @@ public class ConcurrentMapLockStriping {
         //利用多线程验证ConcurrentHashMap与HashMap的性能
         //10个线程负责随机存数据
         doTestConcurrentHashMap(10);
+        doTestHashMap(10);
     }
     /**
      * 执行入口
@@ -53,15 +57,45 @@ public class ConcurrentMapLockStriping {
             long startTime = System.currentTimeMillis();
             startGate.countDown();
             endGate.await();
-            System.out.println("耗时：" + (new Double(System.currentTimeMillis() - startTime) / 1000));
+            System.out.println("org.ybygjy.jcip.chap5.ConcurrentMapLockStriping.doTestConcurrentHashMap(int)#耗时：" + (new Double(System.currentTimeMillis() - startTime) / 1000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    private void doTestHashMap(int nThreads) {
+        final CountDownLatch startGate = new CountDownLatch(1);
+        final CountDownLatch endGate = new CountDownLatch(nThreads);
+        final Map<String, Object> unSynchData = Collections.synchronizedMap(new HashMap<String, Object>());
+//        final Map<String, Object> unSynchData = (new HashMap<String, Object>());
+        for (int i = 1; i <= nThreads; i++) {
+            new Thread("SyncHashMapTestThread_" + i){
+                public void run() {
+                    try {
+                        startGate.await();
+                        try {
+                            new HashMapTest(unSynchData, getName()).run();
+                        } finally {
+                            endGate.countDown();
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
+        }
+        try {
+            long startTime = System.currentTimeMillis();
+            startGate.countDown();
+            endGate.await();
+            System.out.println("org.ybygjy.jcip.chap5.ConcurrentMapLockStriping.doTestHashMap(int)#耗时：" + new Double(System.currentTimeMillis() - startTime) / 1000 );
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
     class ConcurrentHashMapTest implements Runnable {
-        private ConcurrentHashMap<String, Object> concurrentHashMap;
         /** 循环次数*/
-        private static final int MAX_CYCLECOUNT = 1000;
+        private static final int MAX_CYCLECOUNT = 10;
+        private ConcurrentHashMap<String, Object> concurrentHashMap;
         private String threadName;
         public ConcurrentHashMapTest(String threadName, ConcurrentHashMap<String, Object> concurrentHashMap) {
             this.threadName = threadName;
@@ -75,7 +109,32 @@ public class ConcurrentMapLockStriping {
                 this.concurrentHashMap.put(key, String.valueOf(Math.random()));
                 count++;
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    class HashMapTest implements Runnable {
+        /** 循环次数*/
+        private static final int MAX_CYCLECOUNT = 10;
+        private Map<String, Object> hashMap;
+        private String threadName;
+        public HashMapTest(Map<String, Object> hashMap, String threadName) {
+            this.hashMap = hashMap;
+            this.threadName = threadName;
+        }
+
+        @Override
+        public void run() {
+            int count = 1;
+            while (count <= MAX_CYCLECOUNT) {
+                String key = threadName + String.valueOf((int) (Math.random() * 1000));
+                this.hashMap.put(key, String.valueOf(Math.random()));
+                count++;
+                try {
+                    Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
