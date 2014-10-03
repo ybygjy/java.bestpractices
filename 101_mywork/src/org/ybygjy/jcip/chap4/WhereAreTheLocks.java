36407a9da42d1e8454718d43143ea0d548bf0334
ybygjy@gmail.com <ybygjy@gmail.com>
2014-10-03 15:38:23 +0800

    learn jcip

--- 101_mywork/src/org/ybygjy/jcip/chap4/WhereAreTheLocks.java
+++ 101_mywork/src/org/ybygjy/jcip/chap4/WhereAreTheLocks.java
@@ -8,7 +8,18 @@ import java.util.concurrent.CountDownLatch;
 /**
  * 锁在哪里的问题。
  * <p>1、此代码验证客户端正确和不正确加锁的场景</p>
- * <p>2、可以观察到addIfAbsent并未复合我们的要求，比结果可以看到重复值</p>
+ * <p>2、可以观察到addIfAbsent并未符合我们的要求，从结果可以看到重复值</p>
+ * <p>
+ * 3、操作步骤
+ * 3.1、声明一个List的扩展类，该类负责扩展List的功能。
+ * 3.2、在扩展类中新加入addIfAbsent函数，该函数错误的使用了锁，导致扩展的list新特性不具有线程安全性
+ * 3.3、创建测试线程类，负责模拟测试环境
+ * 3.4、创建多个线程，并发读取和写入数据到list的扩展实例中
+ * 3.5、打印list扩展实例数据，观察测试结果
+ * <p>
+ * 4、小结
+ * 4.1、通过添加原子操作来扩展现存类非常脆弱，因为这会将类的加锁代码分布到多个类中。
+ * 4.2、客户端加锁更加脆弱，因为它将原始类的加锁代码放到与原始类完全无关的其它类中。
  * @author WangYanCheng
  * @version 2014年8月13日
  */
@@ -19,7 +30,8 @@ public class WhereAreTheLocks {
 	 * @param args 参数列表
 	 */
 	public static void main(String[] args) {
-		CorrectAndAnCorrectLocked calInst = new CorrectAndAnCorrectLocked();
+		ListHelper calInst = new ListHelper();
+		//声明6个线程
 		for (int i = 0; i < 6; i++) {
 			new TestThread("WhereAreTheLocks_" + i, calInst).start();
 		}
@@ -30,13 +42,13 @@ public class WhereAreTheLocks {
 	 * @version 2014年8月13日
 	 */
 	static class TestThread extends Thread {
-		private CorrectAndAnCorrectLocked calInst;
+		private ListHelper calInst;
 		/**
 		 * 构造函数
 		 * @param threadName 线程名称
-		 * @param calInst {@link CorrectAndAnCorrectLocked}
+		 * @param calInst {@link ListHelper}
 		 */
-		public TestThread(String threadName, CorrectAndAnCorrectLocked calInst) {
+		public TestThread(String threadName, ListHelper calInst) {
 			super(threadName);
 			this.calInst = calInst;
 		}
@@ -65,31 +77,41 @@ public class WhereAreTheLocks {
 		}
 	}
 }
-class CorrectAndAnCorrectLocked {
-	/** 1.声明支持并发操作的list变量*/
-	//TODO 锁在哪里？
+/**
+ * 声明List类的扩展类，该类负责扩展List的功能，但是该类因为使用了错误的锁，所以不具有线程安全性，该类仅仅带来了同步的假象。
+ * @author WangYanCheng
+ * @version 2014年8月13日
+ */
+class ListHelper {
+	/**
+	 * 1.声明支持并发操作的list变量
+	 * 2.注意List在实现客户端加锁或外部加锁时使用同一个锁。
+	 */
 	public List<Object> list = (List<Object>) Collections.synchronizedList(new ArrayList<Object>());
 	/**
 	 * 普通
 	 * @param obj
 	 */
 	public void add(Object obj) {
-		//TODO 锁在哪里？
 		synchronized(list) {
+			if (list.contains(obj)) {
+				return;
+			}
 			list.add(obj);
 		}
 	}
 	/**
 	 * 如果当前list中不存在该元素则加入，否则不做任何操作
+	 * <p>无论list使用哪一个锁来保护它的状态，我们可以确定的是，这个锁并不是</p>
 	 * @param obj
 	 * @return rtnFlag {true:存在未添加,false:不存在已添加}
 	 */
 	public boolean addIfAbsent(Object obj) {
-		//TODO 锁在哪里？
 		synchronized(this) {
 			boolean isAbsent = !this.list.contains(obj);
+			String tmpStr = "T_" + obj;
 			if (!isAbsent) {
-				this.list.add("T_" + obj);
+				this.list.add(tmpStr);
 			}
 			return isAbsent;
 		}
