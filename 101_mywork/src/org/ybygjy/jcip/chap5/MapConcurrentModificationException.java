10cdf0b71b71aa00e7926f3d4347d01ba1e99c7b
ybygjy@gmail.com <ybygjy@gmail.com>
2014-10-20 10:43:06 +0800

    Learn JCIP

--- 101_mywork/src/org/ybygjy/jcip/chap5/MapConcurrentModificationException.java
+++ 101_mywork/src/org/ybygjy/jcip/chap5/MapConcurrentModificationException.java
@@ -1,11 +1,11 @@
 package org.ybygjy.jcip.chap5;
 
-import java.util.Hashtable;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * 验证并发场景下对Map容器的迭代抛出ConcurrentModificationException异常
+ * <p>1.早期的并发容器如HashTable</p>
  * @author WangYanCheng
  * @version 2014年10月19日
  */
@@ -15,12 +15,13 @@ public class MapConcurrentModificationException {
 	 * 构造函数初使化
 	 */
 	public MapConcurrentModificationException() {
-		this.hashTableObj = new Hashtable<String, String>();
+		this.hashTableObj = new HashMap<String, String>();
 	}
 	/**
 	 * 测试入口
 	 */
 	public void doWork() {
+		//创建多个线程负责写入
 		for (int i = 0; i < 3; i++) {
 			new Thread(){
 				public void run() {
@@ -28,7 +29,7 @@ public class MapConcurrentModificationException {
 						String key = String.valueOf(Math.random());
 						hashTableObj.put(key, key);
 						try {
-							sleep(1000);
+							sleep(500);
 						} catch (InterruptedException e) {
 							e.printStackTrace();
 						}
@@ -36,16 +37,19 @@ public class MapConcurrentModificationException {
 				}
 			}.start();
 		}
-		//创建线程
+		//创建多个线程负责迭代容器
 		for (int i = 0; i < 10; i++) {
 			Thread thread = new Thread() {
 				public void run() {
 					while (true) {
+						System.out.println(hashTableObj.toString());
+						/*
 						Iterator<Map.Entry<String, String>> iterator = hashTableObj.entrySet().iterator();
 						while (iterator.hasNext()) {
 							Map.Entry<String, String> entry = iterator.next();
 							System.out.println(entry.getKey() + ":" + entry.getValue());
 						}
+						*/
 						try {
 							sleep(1000);
 						} catch (InterruptedException e) {
